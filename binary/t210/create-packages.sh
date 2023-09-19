201114302ecc610d7bd1bd0901934052bc41ee1d
hmelder <service@hugomelder.com>
2023-09-19 11:56:50 +0200

    create-packages.sh: Fix permissions and enforce root

--- binary/t210/create-packages.sh
+++ binary/t210/create-packages.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
+
+# Check if we are root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
     
 # Get the directory where the script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Arguments: BINARY_DIR
+function fixperm_debian {
+    BINARY_DIR=$1
+
+    chown -R root:root ${BINARY_DIR}/DEBIAN
+    chmod 755 ${BINARY_DIR}/DEBIAN
+    chmod 644 ${BINARY_DIR}/DEBIAN/control
+
+    if test -f ${BINARY_DIR}/DEBIAN/conffiles; then
+        chmod 644 ${BINARY_DIR}/DEBIAN/conffiles
+    fi
+}
+
 function prepare_nv-l4t-drivers {
     echo "INFO: Preparing nv-l4t-drivers package"
 
-    TARBALL="${SCRIPT_DIR}/../nvidia-bin-release/bsp/nv_tegra/nvidia_drivers.tbz2"
+    TARBALL="${SCRIPT_DIR}/../../nvidia-bin-release/bsp/nv_tegra/nvidia_drivers.tbz2"
     DEST="${SCRIPT_DIR}/nv-l4t-drivers"
     tar xjpf $TARBALL -C $DEST
 
@@ -15,11 +34,17 @@ function prepare_nv-l4t-drivers {
     mkdir -p $DEST/usr/lib
     mv $DEST/lib/firmware $DEST/usr/lib
     rm -r $DEST/lib
+
+    # Add Nvidia lib directory to ld configuration
+    echo "/usr/lib/aarch64-linux-gnu/tegra" >> ${DEST}/etc/ld.so.conf.d/nvidia-tegra.conf
+
+    # Fix permissions
+    fixperm_debian $DEST
 }
 
 function main {
     # Check if ../nvidia-bin-release/bsp exists
-    if [ ! -d "${SCRIPT_DIR}/../nvidia-bin-release/bsp" ]; then
+    if [ ! -d "${SCRIPT_DIR}/../../nvidia-bin-release/bsp" ]; then
         echo "ERROR: ${SCRIPT_DIR}/nvidia-bin-release/bsp does not exist!"
         echo "Please run ./download-bsp.sh first."
         exit 1
@@ -29,7 +54,7 @@ function main {
     # Get all directories relative to the script directory
     dirs=("$SCRIPT_DIR"/*/)
 
-    OUTPUT_DIR="$SCRIPT_DIR"/../packages
+    OUTPUT_DIR="$SCRIPT_DIR"/../../packages
     mkdir -p $OUTPUT_DIR
 
     # Run any pre-packaging steps
